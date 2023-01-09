b3820ef234d03586edae3b130b30f3f28c613603
SandyTwix <iamsandytwix@gmail.com>
2023-01-09 22:02:59 +0300

    Initial Commit

---
+++ Tinn/TinnApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinnApp.swift
+//  Tinn
+//
+//  Created by Руслан Трищенков on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TinnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
